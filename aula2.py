5968a4408303896ffb06fbd8e77716c2ae991c62
Luiggi <luiggi.ff@puccampinas.edu.br>
2026-08-28 08:46:10 -0300

    mudei o print

---
+++ aula2.py
@@ -0,0 +1,11 @@
+# programa para aula de TEC TI
+
+val = int(input("Digite o número de colunas: "))
+
+
+for j in range (val):
+   for i in range (val):
+      print("*", end="")
+   print('')
+print ("")
+prfint ("quadrado")
